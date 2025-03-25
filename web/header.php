c5330472b98e9cbb8b289827fbcaa63b6b441ef9
Brothkoff <bitha68@gmail.com>
2025-03-25 21:17:49 +0200

    Add files via upload

---
+++ web/header.php
@@ -0,0 +1,71 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>header</title>
+</head>
+
+<body>
+	<header>
+		<div id="logo">
+			<a href="/home"><img fetchpriority="high" src="images/header.png" width="5334" height="1636" alt="מדלגי העולמות 2025"></a>
+		</div><!-- end of #logo -->
+
+		
+		
+		
+				<div id="sub_menu">
+						</div><!-- end of #sub_menu -->
+			<div id="main-nav" class="main-nav"><div id="responsive_current_menu_item">Home</div><ul id="menu-%d7%a8%d7%90%d7%a9%d7%99" class="menu accessibility_menu"><li id="menu-item-5561" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-5561"><a href="https://tickets.sf-f.org.il/olamot2025/">~התוכניה~</a></li>
+<li id="menu-item-3215" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-3215"><a href="/about/">על הכנס</a>
+<ul class="sub-menu">
+	<li id="menu-item-3846" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3846"><a href="/about/">על הכנס</a></li>
+	<li id="menu-item-3214" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3214"><a href="/about/organizers/">העמותות המארגנות</a></li>
+	<li id="menu-item-3985" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3985"><a href="/info/information/">מודיעין הכנס</a></li>
+	<li id="menu-item-5559" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5559"><a href="/info/access/">נגישות בכנס</a></li>
+	<li id="menu-item-5217" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5217"><a href="/%d7%94%d7%a6%d7%94%d7%a8%d7%aa-%d7%a0%d7%92%d7%99%d7%a9%d7%95%d7%aa/">הצהרת נגישות</a></li>
+</ul>
+</li>
+<li id="menu-item-5459" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-5459"><a href="/volunteer/">התנדבות</a>
+<ul class="sub-menu">
+	<li id="menu-item-5460" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5460"><a href="/volunteer/application/">הרשמה להתנדבות</a></li>
+</ul>
+</li>
+<li id="menu-item-5556" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5556"><a href="/sales/merch/">חנות המזכרות</a></li>
+<li id="menu-item-5587" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-5587"><a href="/areas/">מתחמים בכנס</a>
+<ul class="sub-menu">
+	<li id="menu-item-5588" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5588"><a href="/info/information/">מודיעין הכנס</a></li>
+	<li id="menu-item-5589" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5589"><a href="/sales/register/">קופות</a></li>
+	<li id="menu-item-5590" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5590"><a href="/baggage-hold/">שמירת חפצים</a></li>
+	<li id="menu-item-5591" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5591"><a href="/fair/">יריד הדוכנים</a></li>
+	<li id="menu-item-5592" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5592"><a href="/areas/hobbitton/">הוביטון</a></li>
+	<li id="menu-item-5593" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5593"><a href="/services/games-station/">מתחם משחקי מחשב</a></li>
+	<li id="menu-item-5594" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5594"><a href="/services/community/">המתחם הקהילתי</a></li>
+</ul>
+</li>
+<li id="menu-item-5326" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-5326"><a href="/fair/">יריד הדוכנים</a>
+<ul class="sub-menu">
+	<li id="menu-item-5327" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5327"><a href="/fair/about/">מידע לבעלי דוכנים</a></li>
+</ul>
+</li>
+<li id="menu-item-5294" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-5294"><a href="/story_contest/">תחרות הסיפורים</a>
+<ul class="sub-menu">
+	<li id="menu-item-5296" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-5296"><a href="https://docs.google.com/forms/d/e/1FAIpQLSdHnyC2mrEtiQvnsDZBkTFSAA3QcLOYRQKXUj1jUz3B3Q7xZw/viewform">הגשת סיפור לתחרות הסיפורים</a></li>
+	<li id="menu-item-5295" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5295"><a href="/story_contest/rules/">תקנון תחרות הסיפורים</a></li>
+	<li id="menu-item-5313" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5313"><a href="/story_contest/tips/">טעויות נפוצות בהגשה לתחרות הסיפורים ואיך להמנע מהן</a></li>
+</ul>
+</li>
+<li id="menu-item-5314" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-5314"><a href="/filk/">תחרות "פילק נולד 2025"</a>
+<ul class="sub-menu">
+	<li id="menu-item-5316" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5316"><a href="/filk/rules/">תקנון התחרות "פילק נולד 2025"</a></li>
+	<li id="menu-item-5317" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5317"><a href="/filk/form/">טופס הגשת פילק</a></li>
+	<li id="menu-item-5315" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5315"><a href="/filk/old-filks/">פילקים מתחרויות קודמות</a></li>
+</ul>
+</li>
+<li id="menu-item-3827" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3827"><a href="/about-2/">About</a></li>
+<li id="menu-item-378" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-378"><a href="/contact/">צרו קשר</a></li>
+</ul><a id="responsive_menu_button"></a></div>	
+	</div>
+	</header>
+</body>
+</html>
